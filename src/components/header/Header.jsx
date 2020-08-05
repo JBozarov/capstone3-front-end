7ef95329e8fed298bff0e1111d8b17f58884ccd7
R Devon Brewster <devonbrewster42@gmail.com>
2020-08-05 15:47:55 -0500

    Added toggle to header, updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -31,41 +31,26 @@ const Header = props => {
                   <div className="upper-box" >
                     <h5>Contact Us</h5>
                      {props.location.pathname === '/admin' ? <Link onClick={toggleAdmin} to='/'><h5>Log out</h5></Link> : <Link onClick={toggleAdmin} to='/admin'><h5>Admin login</h5></Link> }
-                     { adminMode ? 
+                     { adminMode === '/admin' ? 
                      <div className={qtyCount ? 'cart-has-item' : null}>
                         <Link to='/cart'><h5>Cart({qtyCount})</h5> </Link>
-<<<<<<< HEAD
                     </div> :
-                    <h5></h5>}
-=======
-                    </div> */}
+                     <h5></h5> }
                   </div>
+                  { adminMode ? 
+
                   <div className="lower-box" >
-                       {/* <ul className="nav-bar"  >
+                       <ul className="nav-bar"  >
                         <li onClick={toggleMenu} onMouseEnter={enterMenu}>MEN'S</li>
                         <li onClick={toggleMenu} onMouseEnter={enterMenu}>WOMEN'S</li>
-                  <li onClick={toggleMenu} onMouseEnter={enterMenu}>KID'S</li> 
-    <li onClick={toggleMenu} onMouseEnter={enterMenu}>DEALS</li> 
+                        <li onClick={toggleMenu} onMouseEnter={enterMenu}>KID'S</li> 
+                        <li onClick={toggleMenu} onMouseEnter={enterMenu}>DEALS</li> 
                      </ul>
-                   {/*  {props.location.pathname === '/tshirts' ? <input className='search-input' value={searchInput} placeholder="Search name or max-price" onChange={e => handleChange(e)} /> : null } */}
-                   {/*  {<input className='search-input' value={searchInput} placeholder="Search" onChange={e => handleChange(e)} /> } */}
->>>>>>> a18a40b86599b4fed24e85a35935192421b31853
-                  </div>
-                   { adminMode ? 
-                     <div className="lower-box" >
-                    
-
-                        <ul className="nav-bar"  >
-                           <li onClick={toggleMenu} onMouseEnter={enterMenu}>MEN'S</li>
-                           <li onClick={toggleMenu} onMouseEnter={enterMenu}>WOMEN'S</li>
-                           <li onClick={toggleMenu} onMouseEnter={enterMenu}>KID'S</li>
-                           <li onClick={toggleMenu} onMouseEnter={enterMenu}>DEALS</li> 
-                        </ul>                     
-                    
-                     {props.location.pathname === '/tshirts' ? <input className='search-input' value={searchInput} placeholder="Search name or max-price" onChange={e => handleChange(e)} /> : null }
-                     {<input className='search-input' value={searchInput} placeholder="Search" onChange={e => handleChange(e)} />}
-                     </div>
-                  : <h2>All Inventory</h2>}
+                    {props.location.pathname === '/tshirts' ? <input className='search-input' value={searchInput} placeholder="Search name or max-price" onChange={e => handleChange(e)} /> : null }
+                    <input className='search-input' value={searchInput} placeholder="Search" onChange={e => handleChange(e)} /> 
+                  </div>             
+                     
+                  : <h2>All Inventory</h2>} 
                </div>
                <div className="desktopDropdownInstanceContainer">
                   <DropdownMenu dropdownActive={ dropdownActive } onMouseEnter={ enterMenu } onMouseLeave={ leaveMenu } toggleMenu={toggleMenu}/>
